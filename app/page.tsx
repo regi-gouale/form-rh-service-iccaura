75bb05a356fd31d4cb0a97e34f0ddcdf6f174abf
Régi GOUALE <regi.gouale@gmail.com>
2024-09-12 15:24:02 +0200

    Refactoriser la configuration Next.js et Tailwind CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+  function handleButtonServeClick() {
+    router.push("/questions");
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -25,22 +34,12 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center justify-center w-full flex-col sm:flex-row">
-          {/* <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deploy now
-          </a> */}
-          <a
+          <Button
+            onClick={handleButtonServeClick}
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 dark:hover:text-black"
-            href="/questions"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             Je veux servir
-          </a>
+          </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
